0e18ca229ff685c069dbd60d128f7260d9ae16e6
Kaz Utashiro <kaz@utashiro.com>
2014-07-02 11:27:51 +0900

    update document and otions in bm.pm

--- lib/App/Greple/bm.pm
+++ lib/App/Greple/bm.pm
@@ -6,6 +6,11 @@ bm - Greple module for bombay format
 
 greple -Mbm [ options ]
 
+    default              --cache-auto
+    --part               '&part($<shift>)'
+    --cdedit             --chdir $ENV{SCCC2DIR}/Edit
+    --ed1                --cdedit --glob '../SCCC1/*.bm1'
+    --ed2                --cdedit --glob '*.bm1'
     --com                find all comments
     --com1               find comment level 1
     --com2               find comment level 2
@@ -20,19 +25,22 @@ greple -Mbm [ options ]
     --eg                 display English block
     --egtxt              display English text block
     --both               display jp/eg combined block
-    --comment            display comment block
     --table              search inside table  
     --figure             search inside figure 
     --example            search inside example
-    --notable            exclude table	 
-    --nofigure           exclude figure 
-    --noexample          exclude example
+    --comment            display comment block
+    --extable            exclude table       
+    --exfigure           exclude figure 
+    --exexample          exclude example
+    --excomment          exclude comment
     --cache-auto         automatic cache update
     --cache-create       create cache
     --cache-clean        remove cache
     --cache-update       force to update cache
     --nocache            disable cache
     --join-block         join block into single line
+    --oldcite            old style 2digit citation
+    --newcite            new style 4digit citation
 
 
 =head1 TEXT FORMAT
@@ -55,35 +63,35 @@ greple -Mbm [ options ]
 Module will read region cache file if it is available and newer than
 target file.  Cache file for file F<FOO> is F<.FOO.greple.bm.json>.
 
-To keep cache files always updated, use option B<-Mbm::cache()>.  It
-will update cache file if it exits and necessary.
+Option B<--cache-auto> is set be default, and cache data is
+automatically updated if necessary.
 
-Use B<-Mbm::create()> to create cache file for the first time.
+Cache data is not created automatically.  Use B<--cache-create> to
+create cache file for the first time.  Use B<--cache-clean> to clean
+up.
 
-Option B<--call=bmcache(command)> is more generic interface.  Use
-B<--call=bmcache(clean)> to clean up cache files, for instance.  Use
-B<--call=bmcache(force,update)> to force update cache even if the
+Option B<--cache-update> can be used to force update cache even if the
 valid cache file exits.
 
 =cut
 
-package App::Greple::bm ;
+package App::Greple::bm;
 
-use utf8 ;
-use strict ;
-use warnings ;
+use utf8;
+use strict;
+use warnings;
 
-use Data::Dumper ;
-use Carp ;
-use File::stat ;
+use Data::Dumper;
+use Carp;
+use File::stat;
 
-my $use_json = 0 ;
-my $mod_json ;
+my $use_json = 0;
+my $mod_json;
 if ($use_json) {
-    eval "use JSON" ;
-    $mod_json = not $@ ;
+    eval "use JSON";
+    $mod_json = not $@;
 } else {
-    $mod_json = 0 ;
+    $mod_json = 0;
 }
 
 my $simple_sane_json = qr{
@@ -117,23 +125,23 @@ unless ($mod_json) {
 }
 
 BEGIN {
-    use Exporter   () ;
-    our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS) ;
+    use Exporter   ();
+    our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
 
-    $VERSION = sprintf "%d.%03d", q$Revision: 1.1 $ =~ /(\d+)/g ;
+    $VERSION = sprintf "%d.%03d", q$Revision: 1.1 $ =~ /(\d+)/g;
 
-    @ISA         = qw(Exporter) ;
-    @EXPORT      = qw(&part &bmcache &join_block) ;
-    %EXPORT_TAGS = ( ) ;
-    @EXPORT_OK   = qw() ;
+    @ISA         = qw(Exporter);
+    @EXPORT      = qw(&part &bmcache &join_block);
+    %EXPORT_TAGS = ( );
+    @EXPORT_OK   = qw();
 }
-our @EXPORT_OK ;
+our @EXPORT_OK;
 
 END { }
 
-my $target = -1 ;
-my %part ;
-my $use_cache = 1 ;
+my $target = -1;
+my %part;
+my $use_cache = 1;
 
 #
 # [\p{East_Asian_Width=Wide}\p{East_Asian_Width=FullWidth}]
@@ -145,12 +153,12 @@ my $wchar_re = qr{
 }x;
 
 sub part {
-    my %arg = @_ ;
+    my %arg = @_;
     my $file = delete $arg{main::FILELABEL} or die;
 
     if ($target != \$_) {
-	setdata($file) ;
-	$target = \$_ ;
+	setdata($file);
+	$target = \$_;
     }
 
     regions(grep { $arg{$_} } keys %arg);
@@ -164,8 +172,8 @@ sub regions {
 }
 
 sub setdata {
-    my $file = shift ;
-    my $nocache = @_ ? shift : 0 ;
+    my $file = shift;
+    my $nocache = @_ ? shift : 0;
 
   CACHECHECK:
     {
@@ -188,54 +196,54 @@ sub setdata {
 	      all => [ [ 0, length ] ],
 	) ;
 
-    my $lang = 'null' ;
+    my $lang = 'null';
 
     ##
     ## comment, eg, jp, both
     ##
     while (m{ ^ ( (?:.+\n)+ (?:.*\z)? )  }mgx) {
-#	my($from, $to) = ( $-[1], $+[1] ) ;
+#	my($from, $to) = ( $-[1], $+[1] );
 	my $to = pos();
 	my $from = $to - length $1;
-	my $para = $1 ;
+	my $para = $1;
 
-	next if $para =~ /\A-\*-/ ;
+	next if $para =~ /\A-\*-/;
 
 	if ($para =~ /\A※/) {
-	    push @{$part{comment}}, [ $from, $to ] ;
-	    $part{$lang}->[-1][1] = $to ;
-	    $part{both}->[-1][1] = $to ;
+	    push @{$part{comment}}, [ $from, $to ];
+	    $part{$lang}->[-1][1] = $to;
+	    $part{both}->[-1][1] = $to;
 	    next;
 	}
 	if ($lang eq 'eg') {
-	    $lang = 'jp' ;
-	    $part{both}->[-1][1] = $to ;
+	    $lang = 'jp';
+	    $part{both}->[-1][1] = $to;
 	} else {
 	    if ($para =~ /$wchar_re/) {
 		warn("Unexpected wide char in english part:\n",
 		     $para,
-		     "***\n") ;
+		     "***\n");
 		if ($lang ne 'eg' and $lang ne 'jp') {
 		    $lang = 'jp';
 		    @{$part{jp}}    or @{$part{jp}}    = ([0, 0]);
 		    @{$part{jptxt}} or @{$part{jptxt}} = ([0, 0]);
 		    @{$part{both}}  or @{$part{both}}  = ([0, 0]);
 		}
-		$part{$lang}->[-1][1] = $to ;
-		$part{both}->[-1][1] = $to ;
+		$part{$lang}->[-1][1] = $to;
+		$part{both}->[-1][1] = $to;
 
-		next ;
+		next;
 	    }
 	    else {
-		$lang = 'eg' ;
-		push @{$part{both}}, [ $from, $to ] ;
+		$lang = 'eg';
+		push @{$part{both}}, [ $from, $to ];
 	    }
 	}
-	push @{$part{$lang}}, [ $from, $to ] ;
-	push @{$part{"${lang}txt"}}, [ $from, $to ] ;
+	push @{$part{$lang}}, [ $from, $to ];
+	push @{$part{"${lang}txt"}}, [ $from, $to ];
 
 	#if ($lang eq 'eg' and $para =~ /$wchar_re/) {
-	#    die "Unexpected wide char in english part:\n", $para ;
+	#    die "Unexpected wide char in english part:\n", $para;
 	#}
     }
 
@@ -249,22 +257,22 @@ sub setdata {
 }
 
 sub bmcache {
-    my %arg = @_ ;
+    my %arg = @_;
     my $file = delete $arg{main::FILELABEL} or die;
-    my $cache_file = cachename($file) ;
+    my $cache_file = cachename($file);
 
     if ($arg{list}) {
-	printf "%s: %s\n", $cache_file, -f $cache_file ? "yes" : "no" ;
+	printf "%s: %s\n", $cache_file, -f $cache_file ? "yes" : "no";
     }
 
-    $arg{nocache} and $use_cache = 0 ;
+    $arg{nocache} and $use_cache = 0;
 
-    $arg{nojson} and $mod_json = 0 ;
+    $arg{nojson} and $mod_json = 0;
 
     if ($arg{clean}) {
 	if (-f $cache_file) {
-	    warn "remove $cache_file\n" ;
-	    unlink $cache_file or warn "$cache_file: $!\n" ;
+	    warn "remove $cache_file\n";
+	    unlink $cache_file or warn "$cache_file: $!\n";
 	}
     }
 
@@ -274,8 +282,8 @@ sub bmcache {
 	and
 	($arg{create} or ($arg{update} and -f $cache_file)))
     {
-	warn "updating $cache_file\n" ;
-	setdata($file, $arg{force}) ;
+	warn "updating $cache_file\n";
+	setdata($file, $arg{force});
 	my $json_text =
 	    to_json(\%part,
 		    { pretty => 0,
@@ -285,10 +293,10 @@ sub bmcache {
 		      allow_nonref => 0,
 		    });
 	if (open CACHE, ">$cache_file") {
-	    print CACHE $json_text, "\n" ;
-	    close CACHE ;
+	    print CACHE $json_text, "\n";
+	    close CACHE;
 	} else {
-	    warn "$cache_file: $!\n" ;
+	    warn "$cache_file: $!\n";
 	}
     }
 }
@@ -302,26 +310,26 @@ sub clean   { bmcache @_, clean => 1   }
 sub list    { bmcache @_, list => 1    }
 
 sub cache_valid {
-    my $file = shift ;
-    my $cache_file = cachename($file) ;
-    my $sc = stat $cache_file ;
-    my $sf = stat $file ;
-    $sc and $sf and $sc->mtime > $sf->mtime ;
+    my $file = shift;
+    my $cache_file = cachename($file);
+    my $sc = stat $cache_file;
+    my $sf = stat $file;
+    $sc and $sf and $sc->mtime > $sf->mtime;
 }
 
 sub cachename {
-    my $file = shift ;
-    my($path, $name) = $file =~ m/((?:.*\/)?)(.*)/ ;
-    my $suffix = ".greple.bm.json" ;
-    $path . "." . $name . $suffix ;
+    my $file = shift;
+    my($path, $name) = $file =~ m/((?:.*\/)?)(.*)/;
+    my $suffix = ".greple.bm.json";
+    $path . "." . $name . $suffix;
 }
 
 sub get_json {
-    my $file = shift ;
+    my $file = shift;
     open(FH, $file) or die;
-    my $json_text = do { local $/; <FH> } ;
-    close FH ;
-    from_json($json_text, {utf8 => 1}) ;
+    my $json_text = do { local $/; <FH> };
+    close FH;
+    from_json($json_text, {utf8 => 1});
 }
 
 ######################################################################
@@ -393,6 +401,8 @@ __DATA__
 
 option default --cache-auto
 
+option --part &part($<shift>)
+
 option --cdedit --chdir $ENV{SCCC2DIR}/Edit
 option --ed1 --cdedit --glob ../SCCC1/*.bm1
 option --ed2 --cdedit --glob *.bm1
@@ -415,21 +425,21 @@ option --egtxt   --block  &part(egtxt)		// display English text block
 
 option --both --block &part(both)		// display jp/eg combined block
 
-option --comment --block &part(comment)		// display comment block
-
 option --table   --inside &part(table)		// search inside table  
 option --figure  --inside &part(figure)		// search inside figure 
 option --example --inside &part(example)	// search inside example
+option --comment --inside &part(comment)	// display comment block
 
-option --notable   --exclude &part(table)	// exclude table	 
-option --nofigure  --exclude &part(figure)	// exclude figure 
-option --noexample --exclude &part(example)	// exclude example
+option --extable   --exclude &part(table)	// exclude table	 
+option --exfigure  --exclude &part(figure)	// exclude figure 
+option --exexample --exclude &part(example)	// exclude example
+option --excomment --exclude &part(comment)	// exclude comment
 
-define :nev: (?=never)match
+define (#nev) (?=never)match
 option --cache-auto   --call bmcache(update)	// automatic cache update
-option --cache-create --re :nev: --call bmcache(create)	// create cache
-option --cache-clean  --re :nev: --call bmcache(clean)	// remove cache
-option --cache-update --re :nev: --call bmcache(force,update) \
+option --cache-create --re (#nev) --call bmcache(create)	// create cache
+option --cache-clean  --re (#nev) --call bmcache(clean)	// remove cache
+option --cache-update --re (#nev) --call bmcache(force,update) \
 						// force to update cache
 option --nocache      --call bmcache(nocache)	// disable cache
 
