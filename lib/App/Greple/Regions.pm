ac0936a12aba5b39175f5231a4f89f0e0fbcc7ff
Kaz Utashiro <kaz@utashiro.com>
2015-01-05 12:43:17 +0900

    Introduce Regions.pm and moved region related functions.

---
+++ lib/App/Greple/Regions.pm
@@ -0,0 +1,183 @@
+package App::Greple::Regions;
+
+use strict;
+use warnings;
+use Carp;
+
+use Exporter 'import';
+our @EXPORT      = qw(REG_IN REG_OUT
+		      match_regions
+		      classify_regions
+		      select_regions
+		      merge_regions
+		      reverse_regions
+		      );
+our %EXPORT_TAGS = ( );
+our @EXPORT_OK   = qw();
+
+use constant {
+    REG_IN => 1, REG_OUT => 0,
+};
+
+sub match_regions {
+    my %arg = @_;
+    my $pattern = $arg{pattern} // croak "Parameter error";
+    my $regex = ref $pattern eq 'Regexp' ? $pattern : qr/$pattern/m;
+    my @regions;
+    while (/$regex/gp) {
+	##
+	## this is much faster than:
+	## my($s, $e) = ($-[0], $+[0]);
+	##
+	## calling pos() cost is not neglective, either.
+	##
+	my $pos = pos();
+	push @regions, [ $pos - length ${^MATCH}, $pos ];
+    }
+    @regions;
+}
+
+sub classify_regions {
+    my $opt = ref $_[0] eq 'HASH' ? shift : {};
+
+    $opt->{strict} and goto &classify_regions_strict;
+
+    my @list = @{+shift};
+    my @by = @{+shift};
+    my @table;
+    for (my $i = 0; $i < @by; $i++) {
+	my($from, $to) = @{$by[$i]};
+	while (@list and $list[0][1] < $from) {
+	    shift @list;
+	}
+	while (@list and $list[0][1] == $from and $list[0][0] < $from) {
+	    shift @list;
+	}
+	my $t = $table[$i] = [];
+	for (my $i = 0; ($i < @list) and ($list[$i][0] < $to); $i++) {
+	    push @$t, [ @{$list[$i]} ];
+	}
+    }
+    @table;
+}
+
+sub classify_regions_strict {
+    my @list = @{+shift};
+    my @by = @{+shift};
+    my @table;
+    for (my $i = 0; $i < @by; $i++) {
+	my($from, $to) = @{$by[$i]};
+	while (@list and $list[0][0] < $from) {
+	    shift @list;
+	}
+	my $t = $table[$i] = [];
+	while (@list and $list[0][0] < $to and $list[0][1] <= $to) {
+	    push @$t, shift @list;
+	}
+    }
+    @table;
+}
+
+sub select_regions {
+    my $opt = ref $_[0] eq 'HASH' ? shift : {};
+
+    $opt->{strict} and goto &select_regions_strict;
+
+    my @list = @{+shift};
+    my @by = @{+shift};
+    my $in_out = shift;
+
+    my(@outside, @inside);
+
+    for (my $i = 0; $i < @by; $i++) {
+	my($from, $to) = @{$by[$i]};
+	for (my $j = 0; $j < @list and $list[$j][0] < $from; $j++) {
+	    push @outside, [ @{$list[$j]} ];
+	}
+	while (@list and $list[0][1] < $from) {
+	    shift @list;
+	}
+	for (my $j = 0; $j < @list and $list[$j][0] < $to; $j++) {
+	    push @inside, [ @{$list[$j]} ];
+	}
+	while (@list and $list[0][1] < $to) {
+	    shift @list;
+	}
+	while (@list and $list[0][0] < $to and $list[0][1] <= $to) {
+	    shift @list;
+	}
+    }
+    push @outside, @list;
+    $in_out == REG_IN ? @inside : @outside;
+}
+
+sub select_regions_strict {
+    my @list = @{+shift};
+    my @by = @{+shift};
+    my $in_out = shift;
+
+    my(@outside, @inside);
+
+    for (my $i = 0; $i < @by; $i++) {
+	my($from, $to) = @{$by[$i]};
+	while (@list and $list[0][0] < $from and $list[0][1] <= $from) {
+	    push @outside, shift(@list);
+	}
+	while (@list and $list[0][0] < $from) {
+	    shift @list;
+	}
+	while (@list and $list[0][0] < $to and $list[0][1] <= $to) {
+	    push @inside, shift(@list);
+	}
+	while (@list and $list[0][0] < $to) {
+	    shift @list;
+	}
+    }
+    push @outside, @list;
+    $in_out == REG_IN ? @inside : @outside;
+}
+
+sub merge_regions {
+    my $option = ref $_[0] eq 'HASH' ? shift : {};
+    my $nojoin = $option->{nojoin};
+    my @in = @_;
+    if ($option->{sort}) {
+	@in = sort({$a->[0] <=> $b->[0] || $b->[1] <=> $a->[1]
+			||  (@$a > 2 ? $a->[2] <=> $b->[2] : 0)
+		   } @in);
+    }
+    my @out;
+    push(@out, shift @in) if @in;
+    while (@in) {
+	my $top = shift @in;
+
+	if ($out[-1][1] > $top->[0]) {
+	    $out[-1][1] = $top->[1] if $out[-1][1] < $top->[1];
+	}
+	elsif (!$nojoin
+	       and $out[-1][1] == $top->[0]
+	       ##
+	       ## don't connect regions in different pattern group
+	       ##
+	       and (@$top < 3 or $out[-1][2] == $top->[2])
+	    ) {
+	    $out[-1][1] = $top->[1] if $out[-1][1] < $top->[1];
+	}
+	else {
+	    push @out, $top;
+	}
+    }
+    @out;
+}
+
+sub reverse_regions {
+    my $from = shift;
+    my $max = shift;
+    my @flat = (0, map(@$_, @$from), $max);
+
+    grep { $_->[0] != $_->[1] }
+    map  { [ splice(@flat, 0, 2) ] }
+    0 .. @flat / 2 - 1;
+}
+
+1;
