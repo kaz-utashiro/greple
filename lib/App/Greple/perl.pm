2ec8fa36a20a5187c37df4ba847d0d6205f552f6
Kaz Utashiro <kaz@utashiro.com>
2015-01-11 12:40:35 +0900

    Rewrote -Mperl module with Regions interface.

--- lib/App/Greple/perl.pm
+++ lib/App/Greple/perl.pm
@@ -14,6 +14,8 @@ greple -Mperl option pattern
     --comment     search from comment part
     --allsection  search from all sections
 
+greple --colordump file
+
 =head1 DESCRIPTION
 
 Sample module for B<greple> command supporting perl script.
@@ -24,65 +26,73 @@ package App::Greple::perl;
 
 use strict;
 use warnings;
-
 use Carp;
+use App::Greple::Common;
+use App::Greple::Regions;
 
-BEGIN {
-    use Exporter   ();
-    our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
+use Exporter 'import';
+our @EXPORT      = qw(part pod comment doc code);
+our %EXPORT_TAGS = ( );
+our @EXPORT_OK   = qw();
 
-    $VERSION = sprintf "%d.%03d", q$Revision: 1.2 $ =~ /(\d+)/g;
 
-    @ISA         = qw(Exporter);
-    @EXPORT      = qw(&pod &comment &podcomment);
-    %EXPORT_TAGS = ( );
-    @EXPORT_OK   = qw();
-}
-our @EXPORT_OK;
+my $target = -1;
+my %part;
 
-END { }
-
-my $pod_re = qr{^=\w+(?s:.*?)(?:\Z|^=cut[ \t]*\n)}m;
+my $pod_re = qr{^=\w+(?s:.*?)(?:\z|^=cut[ \t]*\n)}m;
 my $comment_re = qr{^(?:[ \t]*#.*\n)+}m;
 
-sub pod {
-    my @list;
-    while (/$pod_re/g) {
-	push(@list, [ $-[0], $+[0] ] );
-    }
-    @list;
-}
-sub comment {
-    my @list;
-    while (/$comment_re/g) {
-	push(@list, [ $-[0], $+[0] ] );
-    }
-    @list;
+sub setup {
+    return $target if $target == \$_;
+    my @pod     = match_regions(pattern => $pod_re);
+    my @comment = match_regions(pattern => $comment_re);
+    my @doc     = merge_regions(@pod, @comment);
+    my @code    = reverse_regions(\@doc, length);
+    %part = (
+	pod     => \@pod,
+	comment => \@comment,
+	doc     => \@doc,
+	code    => \@code,
+	);
+    $target = \$_;
 }
 
-sub podcomment {
-    my @list;
-    while (/$pod_re|$comment_re/g) {
-	push(@list, [ $-[0], $+[0] ] );
-    }
-    @list;
+sub part {
+    my %arg = @_;
+    my $file = delete $arg{&FILELABEL} or die;
+    setup and merge_regions do {
+	map  { @{$part{$_}} }
+	grep { exists $part{$_} }
+	grep { $arg{$_} }
+	keys %arg;
+    };
 }
+
+sub pod     { setup and @{$part{pod}} }
+sub comment { setup and @{$part{comment}} }
+sub doc     { setup and @{$part{doc}} }
+sub code    { setup and @{$part{code}} }
 
 1;
 
 __DATA__
 
 define :comment: ^([ \t]*#.*\n)+
-#define :comment: #.*
 define :pod: ^=(?s:.*?)(?:\Z|^=cut[ \t]*\n)
-define :podcomment: :pod:|:comment:
+define :doc: :pod:|:comment:
 
 option --allsection --pod --comment --code
 
-option --pod --inside :pod:
-option --comment --inside :comment:
-option --code --outside :podcomment:
+option --pod     --inside  :pod:
+option --comment --inside  :comment:
+option --code    --outside :doc:
 
-#option --pod --inside '&pod'
-#option --comment --inside '&comment'
-#option --code --outside '&podcomment'
+#option --pod     --inside  '&pod'
+#option --comment --inside  '&comment'
+#option --code    --outside '&doc'
+
+option	--colordump \
+	--le '&part(code)'    --cm=R \
+	--le '&part(comment)' --cm=G \
+	--le '&part(pod)'     --cm=B \
+	--need 1 --all
