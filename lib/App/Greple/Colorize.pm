675fd24725d59348ed5ce5e075f9cdc025759b2b
Kaz Utashiro <kaz@utashiro.com>
2015-01-17 11:46:12 +0900

    Support ANSI 24 grey levels.

--- lib/App/Greple/Colorize.pm
+++ lib/App/Greple/Colorize.pm
@@ -43,6 +43,7 @@ or RGB value if using ANSI 256 color terminal :
     COLOR:
         000 .. 555       : 6 x 6 x 6 216 colors
         000000 .. FFFFFF : 24bit RGB mapped to 216 colors
+	L00 .. L23       : 24 grey levels
 
     Sample:
         005     0000FF        : blue foreground
@@ -57,6 +58,9 @@ and other effects :
     D  Double-struck (boldface)
     F  Flash (blink)
 
+When RGB values are all same in the hex 24bit spec, it is converted 24
+grey levels.
+
 =cut
 
 package App::Greple::Colorize;
@@ -67,9 +71,10 @@ use warnings;
 use Exporter 'import';
 our @EXPORT      = qw();
 our %EXPORT_TAGS = ();
-our @EXPORT_OK   = qw(&colorize);
+our @EXPORT_OK   = qw(colorize);
 
 use Carp;
+use List::Util qw(first);
 use Term::ANSIColor qw(:constants);
 
 use constant { FG => 'fg', BG => 'bg' } ;
@@ -82,8 +87,13 @@ sub colorseq {
 	($start, $end) = split(/:/, $spec, 2);
     } else {
 	map {
-	    while (s/(\/?)((?:[\da-f]{6})|[0-5][0-5][0-5])//i) {
-		$start .= ansi256($2, $1 ? BG : FG);
+	    while (s{(?<bg>   /? ) \K			# /
+		     (?<spec> [0-9a-f]{6}		# 24bit hex
+			    | [0-5][0-5][0-5]		# 216 (6x6x6) colors
+			    | L[01][0-9] | L[2][0-3]	# 24 grey levels
+		     )
+		   }{}xi) {
+		$start .= ansi256($+{spec}, $+{bg} ? BG : FG);
 	    }
 	    $start .= UNDERLINE if /U/;
 	    $start .= REVERSE   if /S/;
@@ -104,24 +114,37 @@ sub colorseq {
     ($start, $end);
 }
 
+my @grey24 = ( 0x08, 0x12, 0x1c, 0x26, 0x30, 0x3a, 0x44, 0x4e,
+	       0x58, 0x62, 0x6c, 0x76, 0x80, 0x8a, 0x94, 0x9e,
+	       0xa8, 0xb2, 0xbc, 0xc6, 0xd0, 0xda, 0xe4, 0xee );
+
 sub ansi256 {
     my($code, $xg) = @_;
-    my($r, $g, $b);
+    my($r, $g, $b, $grey);
     if ($code =~ /^([0-5])([0-5])([0-5])$/) {
 	($r, $g, $b) = ($1, $2, $3);
     }
+    elsif ($code =~ /^L(\d+)/i) {
+	$1 > 23 and croak "Color format error [$code]";
+	$grey = 0 + $1;
+    }
     elsif ($code =~ /^([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/i) {
-	$r = int(5 * hex($1) / 255);
-	$g = int(5 * hex($2) / 255);
-	$b = int(5 * hex($3) / 255);
+	my($rx, $gx, $bx) = (hex($1), hex($2), hex($3));
+	if ($rx != 0 and $rx != 0xff and $rx == $gx and $rx == $bx) {
+	    $grey = first { $rx >= $grey24[$_] } reverse 0 .. $#grey24;
+	} else {
+	    $r = int(5 * $rx / 255);
+	    $g = int(5 * $gx / 255);
+	    $b = int(5 * $bx / 255);
+	}
     }
     else {
-	croak "Color format error.\n";
+	croak "Color format error [$code]";
     }
     sprintf("\e[%d;%d;%dm",
 	    $xg eq BG ? 48 : 38,
 	    5,
-	    16 + ($r * 36) + ($g * 6) + $b
+	    defined $grey ? ($grey + 232) : ($r*36 + $g*6 + $b + 16)
 	);
 }
 
@@ -138,9 +161,11 @@ sub colorize {
 	my($color, $text) = splice @_, 0, 2;
 	$colorcache{$color} //= [ colorseq($color) ];
 	my($s, $e) = @{$colorcache{$color}};
-	$text =~ s/(^|$reset_re)([^\e\r\n]+)/$1$s$2$e/mg if $s ne "";
+	$text =~ s/(^|$reset_re)([^\e\r\n]+)/${1}${s}${2}${e}/mg
+	    if $s ne "";
 	push @result, $text;
     }
+    croak "Wrong number of parameters" if @_;
     join '', @result;
 }
 
