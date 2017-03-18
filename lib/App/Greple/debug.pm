7946518261fbecad354bc13284a4cd4aaffe69d7
Kaz Utashiro <kaz@utashiro.com>
2017-03-18 17:17:34 +0900

    New -Mdebug module.

---
+++ lib/App/Greple/debug.pm
@@ -0,0 +1,57 @@
+=head1 NAME
+
+debug - Greple module for debug control
+
+=head1 SYNOPSIS
+
+greple -Mdebug::on(target)
+
+greple -Mdebug::on=target
+
+=head1 DESCRIPTION
+
+Enable debug mode for specified target.
+
+    getopt: Getopt::Long
+
+    getoptex: Getopt::EX
+
+=head1 EXAMPLE
+
+Next command will show how the module option is processed in
+L<Getopt::EX> module.
+
+    greple -Mdebug::on=getoptex -Mdig --dig . -do something
+
+=cut
+
+
+package App::Greple::debug;
+
+use strict;
+use warnings;
+use Carp;
+
+use App::Greple::Common;
+use Getopt::Long;
+
+my %flags = (
+    getoptex => \$Getopt::EX::Loader::debug,
+    getopt   => sub { main::configure_getopt('debug') },
+);
+
+sub on {
+    my %arg = @_;
+    delete $arg{&FILELABEL}; # no entry when called from -M otption.
+
+    for my $flag (keys %arg) {
+	my $e = $flags{$flag} or next;
+	if (ref $e eq 'CODE') {
+	    $e->($arg{$flag});
+	} else {
+	    ${$flags{$flag}} = $arg{$flag};
+	}
+    }
+}
+
+1;
