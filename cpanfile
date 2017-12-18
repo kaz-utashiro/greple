afc1f352966172acaaaade02e4bc5b021c9f1613
Kaz Utashiro <kaz@utashiro.com>
2017-12-18 16:57:39 +0900

    Use uniq() come with List::Util.

--- cpanfile
+++ cpanfile
@@ -1,7 +1,7 @@
 requires 'perl', '5.014';
 
 requires 'Getopt::EX', 'v1.2.1';
-requires 'List::Util', '1.33';
+requires 'List::Util', '1.45';
 
 on 'test' => sub {
     requires 'Test::More', '0.98';
--- lib/App/Greple/Util.pm
+++ lib/App/Greple/Util.pm
@@ -25,24 +25,6 @@ sub shellquote {
     @_;
 }
 
-push @EXPORT, 'uniq';
-sub uniq {
-    my %seen;
-    grep { not $seen{$_}++ } @_;
-}
-
-##
-## Mimic Text::Glob because it is not in standard library.
-##
-push @EXPORT, 'match_glob';
-sub match_glob {
-    local $_ = shift;
-    s/\?/./g;
-    s/\*/.*/g;
-    my $regex = qr/^$_$/;
-    grep { $_ =~ $regex } @_;
-}
-
 1;
 
 
