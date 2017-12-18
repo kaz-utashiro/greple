332d35b0793e175802eac94590d1e120e0e8faf6
Kaz Utashiro <kaz@utashiro.com>
2017-12-18 17:49:41 +0900

    Checking in changes prior to tagging of version 8.21_01.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index b8b1ff4..10dddc3 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,13 @@ Revision history for Perl application 'greple'.
    
     {{$NEXT}}
    
    +8.21_01 2017-12-18T08:49:26Z
    +
    +    - Require Getopt::EX 1.4.2
    +    - Avoid to use /p modifier and ${^MATCH}.
    +    - Use uniq() come with List::Util.
    +    - Update reverse_regions and wrote test.
    +
     8.21 2017-12-17T04:51:32Z
    
         - Handle beginning single empty line in paragraph mode. (Fixes #6)

--- lib/App/Greple.pm
+++ lib/App/Greple.pm
@@ -9,11 +9,11 @@ greple - extensible grep with lexical expression and region handling
 
 =head1 VERSION
 
-Version 8.21
+Version 8.21_01
 
 =cut
 
-our $VERSION = '8.21';
+our $VERSION = '8.21_01';
 
 =head1 AUTHOR
 
