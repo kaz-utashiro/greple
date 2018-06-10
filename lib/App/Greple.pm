7815a57afb7e592417761f0c5a84f9417fa2bdf0
Kaz Utashiro <kaz@utashiro.com>
2018-06-10 11:21:07 +0900

    Checking in changes prior to tagging of version 8.24.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index a89655e..ea6ef32 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,14 @@ Revision history for Perl application 'greple'.
    
     {{$NEXT}}
    
    +8.24 2018-06-10T02:20:36Z
    +
    +    - Update -Mdebug module.
    +    - Implement --stat option in -Msubst module.
    +    - Add no_ignore_case option to Getopt::Long.
    +    - Modify -Mdig spec to accept multiple directories and find option.
    +    - Option --dm with --uc prints hit count of patterns.
    +
     8.2306 2018-04-11T01:39:31Z
    
         - Fixed bug in Grep.pm.

--- lib/App/Greple.pm
+++ lib/App/Greple.pm
@@ -9,11 +9,11 @@ greple - extensible grep with lexical expression and region handling
 
 =head1 VERSION
 
-Version 8.2306
+Version 8.24
 
 =cut
 
-our $VERSION = '8.2306';
+our $VERSION = '8.24';
 
 =head1 AUTHOR
 
