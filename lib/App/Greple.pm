9f43943acb56a3f29ea8260b74ecc16a16931827
Kaz Utashiro <kaz@utashiro.com>
2018-06-24 15:41:50 +0900

    Checking in changes prior to tagging of version 8.2501.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index ea6ef32..14924b9 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,12 @@ Revision history for Perl application 'greple'.
    
     {{$NEXT}}
    
    +8.2501 2018-06-24T06:41:36Z
    +
    +    - Print matched count when -c is used with -l option.
    +    - Add -do for default debug info.
    +    - Use -ml option with perldoc to print module path.
    +
     8.24 2018-06-10T02:20:36Z
    
         - Update -Mdebug module.

--- lib/App/Greple.pm
+++ lib/App/Greple.pm
@@ -9,11 +9,11 @@ greple - extensible grep with lexical expression and region handling
 
 =head1 VERSION
 
-Version 8.24
+Version 8.2501
 
 =cut
 
-our $VERSION = '8.24';
+our $VERSION = '8.2501';
 
 =head1 AUTHOR
 
