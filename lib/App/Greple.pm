1d827efb8baeecc5dad4d4523dba204c0ed550c3
Kazumasa Utashiro <kaz@utashiro.com>
2020-04-27 13:12:52 +0900

    Checking in changes prior to tagging of version 8.3902.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 236cb39..d5c8590 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl application 'greple'.
     
     {{$NEXT}}
     
    +8.3902 2020-04-27T04:04:40Z
    +
    +    - Use Getopt::EX BASECLASS to search module document.
    +
     8.3901 2020-04-26T07:45:17Z
     
         - Fix --select bug.

--- lib/App/Greple.pm
+++ lib/App/Greple.pm
@@ -9,11 +9,11 @@ greple - extensible grep with lexical expression and region handling
 
 =head1 VERSION
 
-Version 8.3901
+Version 8.3902
 
 =cut
 
-our $VERSION = '8.3901';
+our $VERSION = '8.3902';
 
 =head1 AUTHOR
 
