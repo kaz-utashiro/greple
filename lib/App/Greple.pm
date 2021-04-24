421e5dc9b22f40a2b3d35e4dc3f80c50c13aa14f
Kazumasa Utashiro <kaz@utashiro.com>
2021-04-24 11:38:33 +0900

    Checking in changes prior to tagging of version 8.4301.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 29188e6..4142a71 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,12 @@ Revision history for Perl application 'greple'.
     
     {{$NEXT}}
     
    +8.4301 2021-04-24T02:37:35Z
    +
    +    [ -dfn related update ]
    +    - Use STDERR->printflush for status update.
    +    - Use VT100 ESC 7/8 save/restore cursor sequence.
    +
     8.43 2021-04-04T02:28:49Z
     
         - Update Regions.pm to include generic &filter_regions.

--- lib/App/Greple.pm
+++ lib/App/Greple.pm
@@ -9,11 +9,11 @@ greple - extensible grep with lexical expression and region handling
 
 =head1 VERSION
 
-Version 8.43
+Version 8.4301
 
 =cut
 
-our $VERSION = '8.43';
+our $VERSION = '8.4301';
 
 =head1 AUTHOR
 
