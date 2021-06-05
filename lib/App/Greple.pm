9d7166d21c23848e32848d1938913bf69c60c1fa
Kazumasa Utashiro <kaz@utashiro.com>
2021-06-06 01:19:09 +0900

    Checking in changes prior to tagging of version 8.4401.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 75c4221..728f06d 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,12 @@ Revision history for Perl application 'greple'.
     
     {{$NEXT}}
     
    +8.4401 2021-06-05T16:18:38Z
    +
    +    - Fix --persist STDOUT mode bug.
    +    - Update dig.pm
    +    - Add persist data in -ds stat information.
    +
     8.44 2021-06-04T09:56:35Z
     
         - Add PROGRESS color for -dfn option.

--- lib/App/Greple.pm
+++ lib/App/Greple.pm
@@ -9,11 +9,11 @@ greple - extensible grep with lexical expression and region handling
 
 =head1 VERSION
 
-Version 8.44
+Version 8.4401
 
 =cut
 
-our $VERSION = '8.44';
+our $VERSION = '8.4401';
 
 =head1 AUTHOR
 
