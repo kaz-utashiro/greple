cc32daaa082b0f1c50ff75f703e716fbd8a9c73f
Kazumasa Utashiro <kaz@utashiro.com>
2021-07-02 12:23:30 +0900

    Checking in changes prior to tagging of version 8.46.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index e9ab2c7..cd42380 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,13 @@ Revision history for Perl application 'greple'.
     
     {{$NEXT}}
     
    +8.46 2021-07-02T03:22:51Z
    +
    +    - Interpret "\n" in format string.
    +    - Allow --begin function to die to skip the file.
    +    - Introduce -Mselect module.
    +    - Make -m option to take more than two params.
    +
     8.45 2021-06-11T14:10:38Z
     
         - Replace --persist/--conceal option by --error/--warn.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ greple - extensible grep with lexical expression and region control
 
 # VERSION
 
-Version 8.45
+Version 8.46
 
 # SYNOPSIS
 
--- lib/App/Greple.pm
+++ lib/App/Greple.pm
@@ -9,11 +9,11 @@ greple - extensible grep with lexical expression and region handling
 
 =head1 VERSION
 
-Version 8.45
+Version 8.46
 
 =cut
 
-our $VERSION = '8.45';
+our $VERSION = '8.46';
 
 =head1 AUTHOR
 
