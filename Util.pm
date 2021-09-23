2d0dd92f575cb5e61ce8f4bee238d6b8dd4cda42
Kazumasa Utashiro <kaz@utashiro.com>
2021-09-23 17:14:01 +0900

    Find greple command file from module file location.

--- Util.pm
+++ Util.pm
@@ -3,12 +3,33 @@ use warnings;
 use utf8;
 use open IO => ':utf8', ':std';
 
+use Data::Dumper;
 use Command::Runner;
+
+my $greple_path = sub {
+    my($script, $module) = @_;
+    # Find from $PATH
+    for my $path (split /:+/, $ENV{PATH}) {
+	$path .= "/$script";
+	return $path if -x $path;
+    }
+    # Find from beside module file
+    my $pm_file = $module =~ s/::/\//gr . '.pm';
+    require $pm_file;
+    my $pm_path = $INC{$pm_file};
+    my $install =
+	($pm_path =~ m{(^.*) /lib (?:/[^/]+){0,2} /\Q$pm_file\E$}x)[0]
+	    or die $pm_path;
+    for my $dir (qw(bin script)) {
+	my $file = "$install/$dir/$script";
+	return $file if -f $file;
+    }
+}->('greple', 'App::Greple') or die Dumper \%INC;
 
 sub subst {
     Command::Runner->new(
 	timeout => 2,
-	command => [ $^X, '-Ilib', '-S', 'greple', '-Msubst', @_ ],
+	command => [ $^X, '-Ilib', $greple_path, '-Msubst', @_ ],
 	stderr  => sub { warn "err: $_[0]\n" },
 	)->run;
 }
