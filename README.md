a38226c9b38e344c10531c469756b77e14dfafba
Kazumasa Utashiro <kaz@utashiro.com>
2021-10-20 17:59:49 +0900

    Checking in changes prior to tagging of version 8.48.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 6cbc71f..2090264 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,13 @@ Revision history for Perl application 'greple'.
     
     {{$NEXT}}
     
    +8.48 2021-10-20T08:58:25Z
    +
    +    - Use NO_COLOR for test.
    +    - Update --face handling to use ^ (reset) mark.
    +    - Introduce --uniqsub option.
    +    - Add solarized color in -Mcolors.
    +
     8.47 2021-09-29T08:35:10Z
     
         - Require Getopt::EX to v1.25 to support NO_COLOR and COLORTERM env.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ greple - extensible grep with lexical expression and region control
 
 # VERSION
 
-Version 8.47
+Version 8.48
 
 # SYNOPSIS
 
--- lib/App/Greple.pm
+++ lib/App/Greple.pm
@@ -9,11 +9,11 @@ greple - extensible grep with lexical expression and region handling
 
 =head1 VERSION
 
-Version 8.47
+Version 8.48
 
 =cut
 
-our $VERSION = '8.47';
+our $VERSION = '8.48';
 
 =head1 AUTHOR
 
