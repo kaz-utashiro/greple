529f8ac507760e150ed4818d003db22569582d64
Kazumasa Utashiro <kaz@utashiro.com>
2021-11-03 19:25:52 +0900

    Checking in changes prior to tagging of version 8.50.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index bea5125..1f7d980 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,13 @@ Revision history for Perl application 'greple'.
     
     {{$NEXT}}
     
    +8.50 2021-11-03T10:25:11Z
    +
    +    - Update --matchcount option behavior.
    +
    +    Now --matchcount=3 means exact three, not a minumum.
    +    Takes multiple ranges: --matchcount=,10,20,30,40
    +
     8.49 2021-11-03T01:24:59Z
     
         - Update test code.pm.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ greple - extensible grep with lexical expression and region control
 
 # VERSION
 
-Version 8.49
+Version 8.50
 
 # SYNOPSIS
 
--- lib/App/Greple.pm
+++ lib/App/Greple.pm
@@ -9,11 +9,11 @@ greple - extensible grep with lexical expression and region handling
 
 =head1 VERSION
 
-Version 8.49
+Version 8.50
 
 =cut
 
-our $VERSION = '8.49';
+our $VERSION = '8.50';
 
 =head1 AUTHOR
 
