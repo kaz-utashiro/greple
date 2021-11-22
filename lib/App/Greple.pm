219a82588f4e9ea145eaf0cdc6af9f318e31e8e4
Kazumasa Utashiro <kaz@utashiro.com>
2021-11-23 08:20:59 +0900

    Checking in changes prior to tagging of version 8.51.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 1f7d980..31804e2 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl application 'greple'.
     
     {{$NEXT}}
     
    +8.51 2021-11-22T23:20:07Z
    +
    +    - Now --uc option does not imply case-insensitive color selection.
    +      Use --uniqsub in that case.
    +
     8.50 2021-11-03T10:25:11Z
     
         - Update --matchcount option behavior.

--- lib/App/Greple.pm
+++ lib/App/Greple.pm
@@ -9,11 +9,11 @@ greple - extensible grep with lexical expression and region handling
 
 =head1 VERSION
 
-Version 8.50
+Version 8.51
 
 =cut
 
-our $VERSION = '8.50';
+our $VERSION = '8.51';
 
 =head1 AUTHOR
 
