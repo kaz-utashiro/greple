5658b085db3e6fbbf5a0760ef0c091d7573700d8
Kazumasa Utashiro <kaz@utashiro.com>
2021-12-10 12:41:26 +0900

    Checking in changes prior to tagging of version 8.5204.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 08847a7..eb3c200 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl application 'greple'.
     
     {{$NEXT}}
     
    +8.5204 2021-12-10T03:40:49Z
    +
    +    - Additional fix for shorter block.
    +
     8.5203 2021-12-10T00:18:07Z
     
         - Update --le pattern option handling to keep order.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ greple - extensible grep with lexical expression and region control
 
 # VERSION
 
-Version 8.5203
+Version 8.5204
 
 # SYNOPSIS
 
--- lib/App/Greple.pm
+++ lib/App/Greple.pm
@@ -9,11 +9,11 @@ greple - extensible grep with lexical expression and region handling
 
 =head1 VERSION
 
-Version 8.5203
+Version 8.5204
 
 =cut
 
-our $VERSION = '8.5203';
+our $VERSION = '8.5204';
 
 =head1 AUTHOR
 
