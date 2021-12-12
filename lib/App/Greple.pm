663031dfb629a1f5d24349ac670c6ce5d871a710
Kazumasa Utashiro <kaz@utashiro.com>
2021-12-13 08:49:48 +0900

    Checking in changes prior to tagging of version 8.5205.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index eb3c200..ff6d44d 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl application 'greple'.
     
     {{$NEXT}}
     
    +8.5205 2021-12-12T23:48:57Z
    +
    +    - Make t/11_block.t not to fail by TODO guard.
    +    - Simplify handling shorter block algorithm.
    +
     8.5204 2021-12-10T03:40:49Z
     
         - Additional fix for shorter block.

--- lib/App/Greple.pm
+++ lib/App/Greple.pm
@@ -9,11 +9,11 @@ greple - extensible grep with lexical expression and region handling
 
 =head1 VERSION
 
-Version 8.5204
+Version 8.5205
 
 =cut
 
-our $VERSION = '8.5204';
+our $VERSION = '8.5205';
 
 =head1 AUTHOR
 
