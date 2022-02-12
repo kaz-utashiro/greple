8b10d6c7f14df481bfc3d5afb797c16dfaa36dd8
Kazumasa Utashiro <kaz@utashiro.com>
2022-02-13 00:49:16 +0900

    Checking in changes prior to tagging of version 8.5601.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index e99cfe3..e585be4 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl application 'greple'.
     
     {{$NEXT}}
     
    +8.5601 2022-02-12T15:49:01Z
    +
    +    - Go back to Minilla.
    +
     8.56  2022-02-12 17:47:41 JST
     
         - Switch from Minilla to Milla.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ greple - extensible grep with lexical expression and region control
 
 # VERSION
 
-Version 8.5501
+Version 8.5601
 
 # SYNOPSIS
 
--- lib/App/Greple.pm
+++ lib/App/Greple.pm
@@ -9,11 +9,11 @@ greple - extensible grep with lexical expression and region handling
 
 =head1 VERSION
 
-Version 8.5501
+Version 8.5601
 
 =cut
 
-our $VERSION = '8.5501';
+our $VERSION = '8.5601';
 
 =head1 AUTHOR
 
