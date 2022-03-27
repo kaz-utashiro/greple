5872853c4c05025c989a0cda9ad69c25de9b462c
Kazumasa Utashiro <kaz@utashiro.com>
2022-03-27 10:33:13 +0900

    Checking in changes prior to tagging of version 8.5702.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 1d49da4..098d36f 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl application 'greple'.
     
     {{$NEXT}}
     
    +8.5702 2022-03-27T01:32:48Z
    +
    +    - Fix a bug of reading non-utf8 files.
    +
     8.5701 2022-03-22T00:10:35Z
     
         - Add --git-r option in -Mdig to recurse submodules.

--- lib/App/Greple.pm
+++ lib/App/Greple.pm
@@ -9,11 +9,11 @@ greple - extensible grep with lexical expression and region handling
 
 =head1 VERSION
 
-Version 8.5701
+Version 8.5702
 
 =cut
 
-our $VERSION = '8.5701';
+our $VERSION = '8.5702';
 
 =head1 AUTHOR
 
