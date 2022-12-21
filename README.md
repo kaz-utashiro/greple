059a6adc71c56ac415ea44fad7152eac2d86e2f9
Kazumasa Utashiro <kaz@utashiro.com>
2022-12-21 17:43:01 +0900

    Checking in changes prior to tagging of version 8.60.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index f4cbfae..52ca941 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl application 'greple'.
     
     {{$NEXT}}
     
    +8.60 2022-12-21T08:42:41Z
    +
    +    - Mainly document update.
    +    - Possiblly the final release of Version 8.
    +
     8.59 2022-11-22T08:56:56Z
     
         - Deprecate --require option.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ greple - extensible grep with lexical expression and region control
 
 # VERSION
 
-Version 8.59
+Version 8.60
 
 # SYNOPSIS
 
--- lib/App/Greple.pm
+++ lib/App/Greple.pm
@@ -9,11 +9,11 @@ greple - extensible grep with lexical expression and region handling
 
 =head1 VERSION
 
-Version 8.59
+Version 8.60
 
 =cut
 
-our $VERSION = '8.59';
+our $VERSION = '8.60';
 
 =head1 AUTHOR
 
