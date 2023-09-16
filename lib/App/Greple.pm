a694dfe0fc688c43e98b18b3ae06e76e11ed47e3
Kazumasa Utashiro <kaz@utashiro.com>
2023-09-16 13:33:30 +0900

    Checking in changes prior to tagging of version 9.08.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 52e3108..fc74dce 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,14 @@ Revision history for Perl application 'greple'.
     
     {{$NEXT}}
     
    +9.08 2023-09-16T04:33:12Z
    +
    +    - Update -Mline module
    +      * implement --offload option
    +    - Update -Mdig module
    +      * skip node_modules
    +      * skip "tif" file
    +
     9.07 2023-06-07T12:14:28Z
     
         - Allows multiple --callback functions.

--- lib/App/Greple.pm
+++ lib/App/Greple.pm
@@ -9,11 +9,11 @@ greple - extensible grep with lexical expression and region handling
 
 =head1 VERSION
 
-Version 9.07
+Version 9.08
 
 =cut
 
-our $VERSION = '9.07';
+our $VERSION = '9.08';
 
 =head1 AUTHOR
 
