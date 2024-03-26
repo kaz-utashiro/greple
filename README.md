1bd884f38210fae529fd3898bca5677a60cf7e48
Kazumasa Utashiro <kaz@utashiro.com>
2024-03-27 00:45:42 +0900

    Checking in changes prior to tagging of version 9.12.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 64f4fdd..92a8972 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl application 'greple'.
     
     {{$NEXT}}
     
    +9.12 2024-03-26T15:45:27Z
    +
    +    - update --face option to reset current colormaps
    +
     9.1101 2024-03-10T06:13:21Z
     
         - include total length in stat information

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ greple - extensible grep with lexical expression and region control
 
 # VERSION
 
-Version 9.1101
+Version 9.12
 
 # SYNOPSIS
 
--- lib/App/Greple.pm
+++ lib/App/Greple.pm
@@ -9,11 +9,11 @@ greple - extensible grep with lexical expression and region handling
 
 =head1 VERSION
 
-Version 9.1101
+Version 9.12
 
 =cut
 
-our $VERSION = '9.1101';
+our $VERSION = '9.12';
 
 =head1 AUTHOR
 
