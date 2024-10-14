c19e0a6f665917a02069c42306d5f0b8e392b627
Kazumasa Utashiro <kaz@utashiro.com>
2024-10-14 09:55:11 +0900

    make @color_list and %color_hash accessible from modules.

--- lib/App/Greple/Common.pm
+++ lib/App/Greple/Common.pm
@@ -3,6 +3,48 @@ package App::Greple::Common;
 use v5.14;
 use warnings;
 
+=encoding utf-8
+
+=head1 NAME
+
+App::Greple::Common - interface for common resources
+
+=head1 SYNOPSIS
+
+    use App::Greple::Common qw(@color_list)
+
+=head1 DESCRIPTION
+
+This module provides interface to access L<App::Greple> common
+resources which can be accessed from the external modules.
+
+=head1 EXPORTED VARIABLES
+
+=head2 B<%color_list>
+
+Indexed color pallet.
+
+=head2 B<%color_hash>
+
+Labeled color table.
+
+=head2 B<%debg>
+
+Debug flag.
+
+=head1 AUTHOR
+
+Kazumasa Utashiro
+
+=head1 LICENSE
+
+Copyright ©︎ 2024 Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
 use Exporter 'import';
 our @EXPORT      = ();
 our %EXPORT_TAGS = ();
@@ -11,9 +53,11 @@ our @EXPORT_OK   = qw();
 use constant FILELABEL => '__file__';
 push @EXPORT, qw(FILELABEL);
 
-*opt_d  = \%main::opt_d;		push @EXPORT, qw(%opt_d);
+*debug      = \%::opt_d;	push @EXPORT, qw(%opt_d);
+*color_list = \@::colors;	push @EXPORT, qw(@color_list);
+*color_hash = \%::colormap;	push @EXPORT, qw(%color_hash);
 
-*setopt = \&main::setopt;		push @EXPORT_OK, qw(setopt);
-*newopt = \&main::newopt;		push @EXPORT_OK, qw(newopt);
+*setopt     = \&::setopt;	push @EXPORT_OK, qw(setopt);
+*newopt     = \&::newopt;	push @EXPORT_OK, qw(newopt);
 
 1;
